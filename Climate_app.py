381806eb1ddf240186b6f6c8741e93f0f9cec1ff
cdfencing <65666202+cdfencing@users.noreply.github.com>
2020-08-12 21:08:10 -0500

    climate_app.py changes

--- Climate_app.py
+++ Climate_app.py
@@ -7,3 +7,38 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 
+##################################
+# Database Setup
+##################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# Reflect an existing database into a new model
+Base = automap_base()
+# Reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save the references to the tables
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create session from Python to the Database
+session = Session(engine)
+
+##################################
+# Flask Setup
+##################################
+
+app = Flask(__name__)
+
+
+
+##################################
+# Flask Routes
+##################################
+
+# @app.route("/")
+# def welcome():
+
+
+if __name__ == '__main__':
+    app.run()
